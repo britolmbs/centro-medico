0ae74dcb47b0e613138730c3faba48b976e473fe
Lucas Brito <britolmbs@hotmail.com>
2024-12-05 15:31:32 -0300

    Criando a estrutura do about e colocando discrição no catalogo de serviço da home

--- src/components/Product.js
+++ src/components/Product.js
@@ -6,44 +6,44 @@ const produto = [
     {
         name: 'Consultas Médicas',
         image: '',
-        description: '',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut efficitur metus quis diam luctus condimentum in commodo felis. Sed vitae congue metus, a facilisis odio. Aliquam id tempus arcu. Nulla luctus, tortor lobortis vestibulum pulvinar, orci diam dapibus nisi, sit amet dictum tortor libero at lacus. Fusce mattis lobortis nibh.',
 
     },
 
     {
         name: 'Fisoterapia',
         image: '',
-        description: '',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce interdum erat vel dui semper venenatis. Cras commodo metus in orci molestie, malesuada ullamcorper tortor volutpat. Vestibulum quis mi vestibulum, mattis dolor eget, convallis mi. Integer mattis consectetur lacus at luctus. Cras laoreet velit nisl, ac pellentesque nisi egestas vitae. Quisque.',
 
     },
     {
         name: 'Exames Complementares',
         image: '',
-        description: '',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam consequat ullamcorper purus, id rutrum ipsum ornare at. In hac habitasse platea dictumst. Aenean dictum finibus nibh vel tristique. Donec nulla elit, hendrerit eu viverra at, pulvinar sit amet mi. Donec et lobortis orci, sit amet suscipit nibh. Etiam consequat sit.',
 
     },
     {
         name: 'Laboratório',
         image: '',
-        description: '',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vitae porttitor est. Vestibulum suscipit porta auctor. Nam ut neque leo. Curabitur eu metus dui. Suspendisse hendrerit fermentum nisi et dignissim. Sed commodo porta massa, a lacinia augue molestie quis. Donec ut nisl est. Morbi vestibulum ligula sed lacus luctus, nec.',
 
     },
     {
         name: 'Dentista',
         image: '',
-        description: '',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam urna augue, fringilla in lectus eget, commodo feugiat quam. Etiam semper ante in sapien mollis, non iaculis risus placerat. Nulla mollis justo elit, a pulvinar sem mollis id. Aliquam ut diam ac odio faucibus accumsan. Phasellus lacinia efficitur ultrices. Nunc eleifend.',
 
     },
     {
         name: 'Tratamentos Estéticos',
         image: '',
-        description: '',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget dignissim orci, et dictum tellus. Nulla imperdiet, nulla vel consequat rhoncus, neque nulla interdum ex, non efficitur lacus nisl quis risus. Vestibulum et euismod mi. Proin fermentum gravida malesuada. Cras a ultrices nisl. Aenean sed neque consectetur, fermentum libero eget.',
 
     },
     {
         name: 'Medicina do Trabalho',
         image: '',
-        description: '',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam et mollis purus. Nulla vitae viverra metus, id elementum enim. Aenean bibendum diam at iaculis varius. Mauris id magna elementum, feugiat mi quis, blandit metus. Fusce tincidunt libero sed erat tristique consequat. Maecenas commodo, justo a ornare lobortis, augue lorem vulputate.',
 
     },
 ]
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,6 @@
-import { Container, Typography } from "@mui/material";
+import { Avatar, Container, Grid, Grid2, Typography } from "@mui/material";
 import React from "react";
+
 
 const About = () => {
     return (
@@ -7,9 +8,37 @@ const About = () => {
         <Typography variant="h3" align="center" gutterBottom>
             Sobre Nós
         </Typography>
+
+        <Grid item xs={12} sm={8}>
+            <Typography variant="body1" gutterBottom>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque venenatis dui eu ipsum sollicitudin convallis. Nullam diam libero, consequat id mattis eleifend, interdum at massa. Nam at ipsum quis dui egestas rutrum. Vivamus porta, felis vel volutpat condimentum, metus massa sollicitudin nisi, nec pulvinar nisl nisi sed purus. Duis sagittis nisl sed nulla hendrerit, eget iaculis lorem venenatis. Vestibulum massa nibh, luctus a sagittis vel, convallis aliquam nisl. Pellentesque pulvinar augue quis sapien aliquet molestie.
+
+Nunc hendrerit sit amet lorem in pretium. Aliquam imperdiet id arcu eu congue. Aliquam rhoncus lectus vel lorem posuere semper. Cras non mi nulla. Etiam condimentum odio et sem pulvinar maximus. Vivamus dapibus condimentum mi in luctus. Sed ullamcorper efficitur bibendum. Duis et sem vel lorem bibendum sollicitudin. Duis ligula libero, maximus non fringilla et, maximus a purus. Duis lacinia ex at ultricies sodales. Morbi vehicula nec sem non ornare. Nunc pulvinar velit nec feugiat lobortis. Cras sed tincidunt odio, ac gravida nulla.
+
+Sed interdum ultrices sem, malesuada hendrerit elit interdum eu. Vestibulum ultrices augue et sapien ultrices, ut suscipit ipsum facilisis. Curabitur molestie molestie est ac dapibus. Etiam id dolor dui. Sed ornare luctus ex, quis feugiat sapien mattis in. Sed placerat justo quis.
+            </Typography>
+        </Grid>
+     <Container>
+        <Grid item xs={12} sm={8}>
+            <Typography variant="h4" align="center"
+            gutterBottom>
+                Como começamos
+            </Typography>
+            <Grid container spacing={4} alignItems="center">
+                <Grid item xs={12} sm={6} display="flex" justifyContent="center">
+                    <Avatar alt="centro"
+                    src=""
+                    sx={{ width: 200, height: 200,}} />
+                    <Grid item xs={12} sm={8}>
+                    <Typography variant="body1">adas</Typography>
+                </Grid>
+            </Grid>
+            </Grid>
+            </Grid>
+            </Container>
        </Container>
-    )
-}
+    );
+};
 
 
 export default About;
