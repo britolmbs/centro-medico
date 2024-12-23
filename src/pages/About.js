5215b04ca47b43a61fccb903e34ccb67af5cee0c
Lucas Brito <35507273+britolmbs@users.noreply.github.com>
2024-12-23 14:33:03 +0000

    Melhorado o about.js e product.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,33 +1,58 @@
-import { Image } from "@mui/icons-material";
-import { Avatar, Card, CardMedia, Container, Grid, Grid2, Typography } from "@mui/material";
+import { Container, Grid, Typography, Card, CardContent, CardMedia } from "@mui/material";
 import React from "react";
 
-
 const data = [
-    {id: '1', imagecentro: ''},
-]
+  {
+    id: '1',
+    imagecentro: '', 
+  },
+];
 
 const About = () => {
-    return (
-       <Container id="about" sx={{ py: 4 }}>
-        <Typography variant="h3" align="center" gutterBottom>
-            Sobre Nós
-        </Typography>
+  return (
+    <Container id="about" sx={{ py: 8 }}>
+      <Typography variant="h3" align="center" gutterBottom>
+        Sobre Nós
+      </Typography>
+      <Grid container spacing={4} justifyContent="center">
+        <Grid item xs={12} md={8}>
+          <Card sx={{ display: 'flex', flexDirection: 'column' }}>
+            <CardMedia
+              component="img"
+              height="300"
+              image={data[0].imagecentro}
+              alt="Centro Médico"
+              sx={{ objectFit: 'cover' }}
+            />
+            <CardContent>
+              <Typography variant="body1" gutterBottom>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque venenatis dui eu
+                ipsum sollicitudin convallis. Nullam diam libero, consequat id mattis eleifend,
+                interdum at massa. Nam at ipsum quis dui egestas rutrum. Vivamus porta, felis vel
+                volutpat condimentum, metus massa sollicitudin nisi, nec pulvinar nisl nisi sed
+                purus. Duis sagittis nisl sed nulla hendrerit, eget iaculis lorem venenatis.
+                Vestibulum massa nibh, luctus a sagittis vel, convallis aliquam nisl. Pellentesque
+                pulvinar augue quis sapien aliquet molestie.
 
-        <Grid item xs={12} sm={8}>
-            <Typography variant="body1" gutterBottom>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque venenatis dui eu ipsum sollicitudin convallis. Nullam diam libero, consequat id mattis eleifend, interdum at massa. Nam at ipsum quis dui egestas rutrum. Vivamus porta, felis vel volutpat condimentum, metus massa sollicitudin nisi, nec pulvinar nisl nisi sed purus. Duis sagittis nisl sed nulla hendrerit, eget iaculis lorem venenatis. Vestibulum massa nibh, luctus a sagittis vel, convallis aliquam nisl. Pellentesque pulvinar augue quis sapien aliquet molestie.
+                Nunc hendrerit sit amet lorem in pretium. Aliquam imperdiet id arcu eu congue.
+                Aliquam rhoncus lectus vel lorem posuere semper. Cras non mi nulla. Etiam condimentum
+                odio et sem pulvinar maximus. Vivamus dapibus condimentum mi in luctus. Sed
+                ullamcorper efficitur bibendum. Duis et sem vel lorem bibendum sollicitudin. Duis
+                ligula libero, maximus non fringilla et, maximus a purus. Duis lacinia ex at
+                ultricies sodales. Morbi vehicula nec sem non ornare. Nunc pulvinar velit nec feugiat
+                lobortis. Cras sed tincidunt odio, ac gravida nulla.
 
-Nunc hendrerit sit amet lorem in pretium. Aliquam imperdiet id arcu eu congue. Aliquam rhoncus lectus vel lorem posuere semper. Cras non mi nulla. Etiam condimentum odio et sem pulvinar maximus. Vivamus dapibus condimentum mi in luctus. Sed ullamcorper efficitur bibendum. Duis et sem vel lorem bibendum sollicitudin. Duis ligula libero, maximus non fringilla et, maximus a purus. Duis lacinia ex at ultricies sodales. Morbi vehicula nec sem non ornare. Nunc pulvinar velit nec feugiat lobortis. Cras sed tincidunt odio, ac gravida nulla.
-
-Sed interdum ultrices sem, malesuada hendrerit elit interdum eu. Vestibulum ultrices augue et sapien ultrices, ut suscipit ipsum facilisis. Curabitur molestie molestie est ac dapibus. Etiam id dolor dui. Sed ornare luctus ex, quis feugiat sapien mattis in. Sed placerat justo quis.
-            </Typography>
+                Sed interdum ultrices sem, malesuada hendrerit elit interdum eu. Vestibulum ultrices
+                augue et sapien ultrices, ut suscipit ipsum facilisis. Curabitur molestie molestie
+                est ac dapibus. Etiam id dolor dui. Sed ornare luctus ex, quis feugiat sapien mattis
+                in. Sed placerat justo quis.
+              </Typography>
+            </CardContent>
+          </Card>
         </Grid>
-</Container>
-       
-       
-    );
+      </Grid>
+    </Container>
+  );
 };
-
 
 export default About;
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,57 +1,63 @@
-import  Box  from "@mui/material/Box";
-import { Card, CardContent, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography } from "@mui/material";
+import Box from "@mui/material/Box";
+import {
+  Card,
+  CardContent,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Typography,
+} from "@mui/material";
 import { useState } from "react";
 
-const Products =  () =>{
-    const [selecionarServico, setSelecionarServico] = useState(null);
+const Products = () => {
+  const [selecionarServico, setSelecionarServico] = useState(null);
 
-    const handleListItemClick = (servico) => {
-        setSelecionarServico(servico === selecionarServico ? null : servico);
-    };
+  const handleListItemClick = (servico) => {
+    setSelecionarServico(servico === selecionarServico ? null : servico);
+  };
 
-    const servicos = [
-        "Clinica Médica",
-        "Fisoterapia",
-        "Exames Complementares",
-        "Psicologia",
-        "Dentista",
-        "Tratamentos Estéticos",
-        "Medicina do Trabalho",
-    ];
+  const servicos = [
+    "Clinica Médica",
+    "Fisoterapia",
+    "Exames Complementares",
+    "Psicologia",
+    "Dentista",
+    "Tratamentos Estéticos",
+    "Medicina do Trabalho",
+  ];
 
-    return (
-       <Box sx={{ Width: '100%', maxWidth: 360, bgcolor: 'background.paper'}}> 
-       <nav aria-label="main mailbox folders">
-        <List>
+  return (
+    <Box sx={{ display: "flex", justifyContent: "center", alignItems: "flex-start", width: "100%" }}>
+      <Box sx={{ width: "20%", minWidth: 200, paddingRight: 2 }}>
+        <nav aria-label="main mailbox folders">
+          <List>
             {servicos.map((servico, index) => (
-                <ListItem key={index} disablePadding>
-                    <ListItemButton onClick={() => handleListItemClick(servico)}>
-                        <ListItemText primary={servico} />
-                    </ListItemButton>
-                </ListItem>
+              <ListItem key={index} disablePadding>
+                <ListItemButton onClick={() => handleListItemClick(servico)}>
+                  <ListItemText primary={servico} />
+                </ListItemButton>
+              </ListItem>
             ))}
-        </List>
+          </List>
         </nav>
-
+      </Box>
+      <Box sx={{ width: "60%", display: "flex", justifyContent: "center" }}>
         {selecionarServico && (
-            <Card sx={{ marginTop: 2}}>
-                <CardContent>
-                    <Typography variant="h6" component="div">
-                        {selecionarServico}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        {selecionarServico}
-                    </Typography>
-                </CardContent>
-            </Card>
+          <Card sx={{ width: "100%", maxWidth: 400, margin: 2 }}>
+            <CardContent>
+              <Typography variant="h6" component="div">
+                {selecionarServico}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {selecionarServico}
+              </Typography>
+            </CardContent>
+          </Card>
         )}
-       
-       
-       </Box>
-      
-    );
+      </Box>
+    </Box>
+  );
 };
-
-
 
 export default Products;
