f5a38354869305cb7cc6e262631ecc7d611e4b0b
Lucas Brito <britolmbs@hotmail.com>
2024-12-29 11:34:10 -0300

    colocando descrição no Sobre nos

--- src/pages/About.js
+++ src/pages/About.js
@@ -26,26 +26,7 @@ const About = () => {
             />
             <CardContent>
               <Typography variant="body1" gutterBottom>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque venenatis dui eu
-                ipsum sollicitudin convallis. Nullam diam libero, consequat id mattis eleifend,
-                interdum at massa. Nam at ipsum quis dui egestas rutrum. Vivamus porta, felis vel
-                volutpat condimentum, metus massa sollicitudin nisi, nec pulvinar nisl nisi sed
-                purus. Duis sagittis nisl sed nulla hendrerit, eget iaculis lorem venenatis.
-                Vestibulum massa nibh, luctus a sagittis vel, convallis aliquam nisl. Pellentesque
-                pulvinar augue quis sapien aliquet molestie.
-
-                Nunc hendrerit sit amet lorem in pretium. Aliquam imperdiet id arcu eu congue.
-                Aliquam rhoncus lectus vel lorem posuere semper. Cras non mi nulla. Etiam condimentum
-                odio et sem pulvinar maximus. Vivamus dapibus condimentum mi in luctus. Sed
-                ullamcorper efficitur bibendum. Duis et sem vel lorem bibendum sollicitudin. Duis
-                ligula libero, maximus non fringilla et, maximus a purus. Duis lacinia ex at
-                ultricies sodales. Morbi vehicula nec sem non ornare. Nunc pulvinar velit nec feugiat
-                lobortis. Cras sed tincidunt odio, ac gravida nulla.
-
-                Sed interdum ultrices sem, malesuada hendrerit elit interdum eu. Vestibulum ultrices
-                augue et sapien ultrices, ut suscipit ipsum facilisis. Curabitur molestie molestie
-                est ac dapibus. Etiam id dolor dui. Sed ornare luctus ex, quis feugiat sapien mattis
-                in. Sed placerat justo quis.
+              No coração de Santa Cruz, nosso centro médico tem se dedicado desde 2022 a oferecer serviços de saúde de alta qualidade com um toque humano. Especializados em clínica médica, exames laboratoriais, odontologia e tratamentos estéticos, nos orgulhamos de proporcionar cuidados abrangentes e personalizados a preços acessíveis. Nosso compromisso é com o bem-estar e a satisfação de nossos pacientes, garantindo que cada visita seja uma experiência positiva e acolhedora. Venha nos conhecer e descubra como podemos cuidar de você e da sua saúde com excelência e carinho.
               </Typography>
             </CardContent>
           </Card>
