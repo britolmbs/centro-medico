5c5d592aeb87fcc78ff036c7fc4dcf0355bc093a
Lucas Brito <35507273+britolmbs@users.noreply.github.com>
2025-01-09 10:34:54 -0300

    Colocando a cor no footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <footer style={{ marginTop: 'auto', padding: '1rem 0'}}>
+        <footer style={{ marginTop: 'auto', padding: '1rem 0' background: '#24244f', color: '#828ba6'}}>
             <Container>
                 <Typography variant="body1" align="center">
                 © 2024 Lucas Brito | Centro Médico
